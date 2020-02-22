b4f8abe71712773d7f6a9c058a380732e8e628d2
john ruble <jrockwv@gmail.com>
2020-02-21 21:46:32 -0500

    just a random commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header.js";
-
+import axios from 'axios';
 
 export default function App() {
+const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character/')
+    .then(response => {
+      setCharacters(response.data.results)
+    })
+    .catch(error => console.log('oops you got an error', error));
+  }, []);
   return (
     <main>
       <Header />
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,26 @@
 import React from "react";
+import styled from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+
+const CardContainer= styled.div`
+display: flex;
+align-items: center;
+margin: 15px;
+padding: 15px;
+width: 400px;
+border-radius: 20px;
+h2{
+}
+img{
+  border:3px solid green;
+}
+`
+
+export default function CharacterCard({character}) {
+  return (
+    <CardContainer>
+      <h2>{character.name}</h2>
+      <img src= {character.image}/>
+    </CardContainer>
+  )
 }
